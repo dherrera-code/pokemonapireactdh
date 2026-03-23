d68ba67b6c38c29d483503fb90f022d0cb0d9770
dherrera-code <dherr929@gmail.com>
2026-03-23 15:25:30 -0700

    update my readme!

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center  bg-[url('/assets/Pokeball-bg.png')] bg-cover h-screen font-sans ">
+    <div className="flex flex-col flex-1 items-center justify-center  bg-[url('/assets/Pokeball-bg.png')] bg-cover bg-center h-screen font-sans ">
       <main className="">
         <h1>Hello Pokemon World!!!</h1>
         <FavoritesDrawer key={"left"}></FavoritesDrawer>
