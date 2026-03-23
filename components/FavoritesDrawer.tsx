f9d2eb2a0ad8679ef5d4fc6ff408b99fcf040e52
dherrera-code <dherr929@gmail.com>
2026-03-23 15:22:44 -0700

    initialize repo for pokemon API with shadCn for drawer component!

---
+++ components/FavoritesDrawer.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer'
+import { Button } from './ui/button'
+
+const FavoritesDrawer = () => {
+    return (
+        <div>
+            <Drawer direction='left'>
+                <DrawerTrigger className=''>Open</DrawerTrigger> 
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                        <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button>Submit</Button>
+                        <DrawerClose>
+                            <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </div>
+    )
+}
+
+export default FavoritesDrawer
